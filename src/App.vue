65d2ad690755171739a1f4deca48c1d8b0382664
Diego Pertusa <mrjeffworker@MacBook-Pro-de-MRJEFF-6.local>
2022-01-21 14:07:10 +0100

    add config

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,32 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/Header.vue";
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <Header />
 </template>
 
 <style lang="postcss">
+:root {
+  --primary-font: "Playfair Display", baskerville, serif;
+  --secondary-font: "Montserrat", helvetica, arial, sans-serif;
+  --primary-color: #274035;
+  --text-color: #706e67;
+  --background-color: #f3f2f1;
+}
+
 body {
-  background-color: #f3f2f1;
+  background-color: var(--background-color);
 }
 
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: var(--primary-font);
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: var(--text-color);
   margin-top: 60px;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1 class="header__title">ANA / DIEGO</h1>
+  <h2 class="header__subtitle">Huerto Bonet · 12/06/2022 · 12:45</h2>
+</template>
+
+<style scoped lang="postcss">
+.header__title {
+  font-family: var(--primary-font);
+  font-size: 84px;
+  font-weight: bold;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  color: var(--primary-color);
+}
+
+.header__subtitle {
+  font-family: var(--secondary-font);
+  font-size: 26px;
+  font-weight: 300;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  color: var(--text-color);
+}
+</style>
