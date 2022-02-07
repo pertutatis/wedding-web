1153b225a95437773c02fe4c82e1f8465264a66e
Diego Pertusa <mrjeffworker@MacBook-Pro-de-MRJEFF-6.local>
2022-02-07 12:01:39 +0100

    add stream

--- src/api/http.ts
+++ src/api/http.ts
@@ -5,7 +5,42 @@ export const HTTP = (url:string, body:string, method:string) => {
     body
   })
   .then(response => {
-    return response.body ? response.body : response;
+    return response.body
+  })
+  .then(rb => {
+    if (rb == null) return rb
+    
+    const reader = rb.getReader();
+
+    return new ReadableStream({
+      start(controller) {
+        // The following function handles each data chunk
+        function push() {
+          // "done" is a Boolean and value a "Uint8Array"
+          reader.read().then( ({done, value}) => {
+            // If there is no more data to read
+            if (done) {
+              controller.close();
+              return;
+            }
+            // Get the data and send it to the browser via the controller
+            controller.enqueue(value);
+            // Check chunks by logging to the console
+            push();
+          })
+        }
+
+        push();
+      }
+    });
+  })
+  .then(stream => {
+    // Respond with our stream
+    return new Response(stream, { headers: { "Content-Type": "text/html" } }).json();
+  })
+  .then(result => {
+    // Do things with result
+    return result;
   })
   .catch(err => { throw err })
 }
