4dee3b4062dd96a52e016d750363c77e63d84809
Diego Pertusa <mrjeffworker@MacBook-Pro-de-MRJEFF-6.local>
2022-02-17 10:07:47 +0100

    update readme

--- src/api/storyblock.ts
+++ src/api/storyblock.ts
@@ -1,6 +1,7 @@
 import StoryblokClient from 'storyblok-js-client';
 
 export const Storyblok = new StoryblokClient({
+  // peding: add to env file
   accessToken: 'QrGQXYvpwTA3ujnHNbSjngtt',
   cache: {
     clear: 'auto',
